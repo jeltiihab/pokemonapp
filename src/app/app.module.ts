4e20a498f1629198a2fcf98b6455747f0b69f696
User Insta <ihab.jelti@gmail.com>
2022-09-09 11:09:12 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { InMemoryDataService } from './in-memory-data.service';
+import { LoginComponent } from './login/login.component';
+
+// import { environment } from 'src/environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import {  AngularFirestoreModule} from '@angular/fire/compat/firestore';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,  { dataEncapsulation: false }),
+    PokemonModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
+  providers: [],
+  // { provide: AngularFirestoreSettingsToken, useValue:{}}
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'pokemon-441c5',
+    appId: '1:1005476196585:web:e6b75f1d287872f1ab8078',
+    databaseURL: 'https://pokemon-441c5-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pokemon-441c5.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCP0QyqbHivDFq6nPz2xFr5tbPn5k87w1I',
+    authDomain: 'pokemon-441c5.firebaseapp.com',
+    messagingSenderId: '1005476196585',
+    measurementId: 'G-32150EG340',
+  },
+  production: true
+};
